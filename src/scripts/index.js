8db4a09b17cace2cdeadb689243c12ff3d232589
Artem Voevodin <voevodin320@gmail.com>
2024-06-06 23:00:44 +0200

    moduled

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,16 +3,14 @@ import '../pages/index.css';
 const logo = document.querySelector('.logo__img-wrapper');
 
 logo.addEventListener('mouseover', () => {
-  if(!logo.classList.contains('animated-rotate')) {
+  if(!logo.classList.contains('animation-rotate')) {
     
-    logo.classList.add('animated-rotate');
+    logo.classList.add('animation-rotate');
 
     setTimeout(() => {
-      logo.classList.remove('animated-rotate');
+      logo.classList.remove('animation-rotate');
     }, 2000);
   }
 })
-
-
 
 
