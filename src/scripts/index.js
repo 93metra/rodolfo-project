360ea30c0278b73c69f137ad32a9b383fc2f43fc
Artem Voevodin <voevodin320@gmail.com>
2024-07-03 21:35:58 +0200

    change language buttons and logic added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -67,5 +67,60 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // ================== lang ========================
-document.querySelector('.lng-caption').textContent = langArr['lng-caption']['es']
-document.querySelector('.lng-phase_one').textContent = langArr['lng-phase_one']['en']
+// document.querySelector('.lng-caption').textContent = langArr['lng-caption']['es']
+// document.querySelector('.lng-phase_one').textContent = langArr['lng-phase_one']['en']
+// document.querySelector('.lng-slogan').textContent = langArr['lng-slogan']['en']
+// document.querySelector('.lng-slogan__span_one').textContent = langArr['lng-slogan__span_one']['en']
+
+const esButton = document.querySelector('.language__button_es');
+const enButton = document.querySelector('.language__button_en');
+
+esButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  esButton.classList.toggle('language__button_active');
+  enButton.classList.toggle('language__button_active');
+  checkLanguage();
+})
+
+enButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  enButton.classList.toggle('language__button_active');
+  esButton.classList.toggle('language__button_active');
+  checkLanguage();
+})
+
+// function checkLanguage() {
+//   if(esButton.classList.contains('language__button_active')) {
+//     console.log('es');
+//     for(let key in langArr) {
+//       document.querySelector(key);
+//       console.log(key);
+//     }
+//   } else if(enButton.classList.contains('language__button_active')) {
+//     console.log('en');
+//     for(let key in langArr) {
+//       document.querySelector(key);
+//       console.log(key);
+//     }
+//   }
+// }
+
+function checkLanguage() {
+  let lang = 'en';
+  if(esButton.classList.contains('language__button_active')) {
+    lang = 'es';
+  } else if(enButton.classList.contains('language__button_active')) {
+    lang = 'en';
+  }
+
+  for(let key in langArr) {
+    const elements = document.querySelectorAll(`.${key}`);
+    elements.forEach(element => {
+      if (langArr[key][lang]) {
+        element.innerText = langArr[key][lang];
+      }
+    });
+  }
+}
+
+checkLanguage()
--- src/scripts/lang.js
+++ src/scripts/lang.js
@@ -1,29 +1,174 @@
+// export const langArr = {
+//   'lng-caption': {
+//     'es': 'La primera memecoin de comunidad 100% hispana en Solana. La primera memecoin de comunidad 100% hispana en Solana. La primera memecoin de comunidad 100% hispana en Solana.',
+//     'en': 'The first memecoin',
+//   },
+
+//   'lng-intro__title': {
+//     'es': 'Quien es RODOLFO?',
+//     'en': '',
+//   },
+//   'lng-intro_one': {
+//     'es': 'Nuestro objetivo principal es crear una comunidad fuerte e involucrada antes de lanzar a $RODOLFO al mercado global.',
+//     'en': '',
+//   },
+//   'lng-intro_two': {
+//     'es': 'RODOLFO es la primera memecoin enfocada a la comunidad hispana que busca revolucionar y expandir la forma en la que vemos las criptomonedas.',
+//     'en': '',
+//   },
+//   'lng-intro_three': {
+//     'es': 'Queremos crear un movimiento, no una memecoin cualquiera que sera olvidada desues de una semana.',
+//     'en': '',
+//   },
+
+//   'lng-slogan': {
+//     'es': 'Queremos hacer historia en la comunidad hispana.',
+//     'en': 'We want to make',
+//   },
+
+//   'lng-phase_one': {
+//     'es': 'Fase 1',
+//     'en': 'Phase 1',
+//   },
+//   'lng-web': {
+//     'es': 'Construir una web',
+//     'en': '',
+//   },
+//   'lng-telegram': {
+//     'es': 'Crear nuestro telegram',
+//     'en': '',
+//   },
+//   'lng-tweeter': {
+//     'es': 'Crear nuestro tweeter',
+//     'en': '',
+//   },
+//   'lng-instagram': {
+//     'es': 'Crear nuestro Instagram',
+//     'en': '',
+//   },
+//   'lng-marketing': {
+//     'es': 'Empezar con las primeras campanas de marketing organicas',
+//     'en': '',
+//   },
+
+//   'lng-phase_two': {
+//     'es': 'Fase 2',
+//     'en': 'Phase 2',
+//   },
+//   'lng-presale': {
+//     'es': 'Realizar la Preventa',
+//     'en': '',
+//   },
+//   'lng-liquid': {
+//     'es': 'Crear el Liquidity Pol y quemarlo al instante',
+//     'en': '',
+//   },
+//   'lng-solana': {
+//     'es': 'Lanzamiento de $RODOLFO en Solana',
+//     'en': '',
+//   },
+//   'lng-dex': {
+//     'es': 'Actualizar todos los datos en DexScreener',
+//     'en': '',
+//   },
+//   'lng-jupiter': {
+//     'es': 'Verificar nuestro token in RaYdium y Jupiter',
+//     'en': '',
+//   },
+//   'lng-pay': {
+//     'es': 'Marketing de pago en DexScreener',
+//     'en': '',
+//   },
+
+//   'lng-phase_three': {
+//     'es': 'Fase 3',
+//     'en': 'Phase 3',
+//   },
+//   'lng-promise': {
+//     'es': 'Realizar los objetivos segun el mardet cap alcanzado',
+//     'en': '',
+//   },
+//   'lng-social': {
+//     'es': 'Realizar campanas de marketing de pago en redes sociales',
+//     'en': '',
+//   },
+//   'lng-democrat': {
+//     'es': 'Votaciones con los miembros de la comunidad sobre la expansion del proyecto',
+//     'en': '',
+//   },
+
+//   'lng-teem': {
+//     'es': 'Equipo y Developers',
+//     'en': '',
+//   },
+//   'lng-token_pres': {
+//     'es': 'Preventa',
+//     'en': '',
+//   },
+//   'lng-token_mark': {
+//     'es': 'Cartera de Marketing',
+//     'en': '',
+//   },
+
+//   'lng-plans__title': {
+//     'es': 'Planes segun la capitalizacion',
+//     'en': '',
+//   },
+//   'lng-plans_one': {
+//     'es': '- Abriremos nuestra tienda de marketing con numerosos disenos de RODOLFO',
+//     'en': '',
+//   },
+//   'lng-plans_two': {
+//     'es': '- Comenzaremos con sorteos para los holders de RODOLFO',
+//     'en': '',
+//   },
+//   'lng-plans_three': {
+//     'es': '- Crear numerosas vallas ublicitarias con la imagen de RODOLFO junto con campanas con influencers',
+//     'en': '',
+//   },
+//   'lng-plans_four': {
+//     'es': '- Votaciones con los holders para decidir el futuro del proyecto',
+//     'en': '',
+//   },
+
+//   'lng-footer_disc': {
+//     'es': ' $RODOLFO es una memecoin sin valor intriseco ni expectativa de rendimiento financiero. $RODOLFO es completamente inutil y se ha creado con fines recreativos y de entretenimiento. Al comprar $RODOLFO, aceptas haber leido este descargo de responsabilidad.',
+//     'en': '',
+//   },
+//   'lng-footer_span': {
+//     'es': '®Aviso legal:',
+//     'en': '',
+//   },
+// }
+
+// ==============================================
+
 export const langArr = {
   'lng-caption': {
-    'es': 'La primera memecoin de comunidad 100% hispana en Solana. La primera memecoin de comunidad 100% hispana en Solana. La primera memecoin de comunidad 100% hispana en Solana.',
-    'en': 'The first memecoin',
+    'es': 'La primera memecoin de comunidad hispana en Solana. La primera memecoin de comunidad hispana en Solana. La primera memecoin de comunidad hispana en Solana.',
+    'en': 'The first Hispanic community memecoin on Solana. The first Hispanic community memecoin on Solana. The first Hispanic community memecoin on Solana.',
   },
 
   'lng-intro__title': {
     'es': 'Quien es RODOLFO?',
-    'en': '',
+    'en': 'Who is RODOLFO?',
   },
   'lng-intro_one': {
     'es': 'Nuestro objetivo principal es crear una comunidad fuerte e involucrada antes de lanzar a $RODOLFO al mercado global.',
-    'en': '',
+    'en': 'Our main goal is to create a strong and engaged community before launching $RODOLFO to the global market.',
   },
   'lng-intro_two': {
     'es': 'RODOLFO es la primera memecoin enfocada a la comunidad hispana que busca revolucionar y expandir la forma en la que vemos las criptomonedas.',
-    'en': '',
+    'en': 'RODOLFO is the first memecoin focused on the Hispanic community, aiming to revolutionize and expand the way we view cryptocurrencies.',
   },
   'lng-intro_three': {
     'es': 'Queremos crear un movimiento, no una memecoin cualquiera que sera olvidada desues de una semana.',
-    'en': '',
+    'en': 'We want to create a movement, not just another memecoin that will be forgotten after a week.',
   },
 
-  'need to check what to do with the spans in intro__slogan': {
-    'es': 'Quien es RODOLFO?',
-    'en': '',
+  'lng-slogan': {
+    'es': 'Queremos hacer historia en la comunidad hispana.',
+    'en': 'We want to make history in the Hispanic community.',
   },
 
   'lng-phase_one': {
@@ -32,23 +177,23 @@ export const langArr = {
   },
   'lng-web': {
     'es': 'Construir una web',
-    'en': '',
+    'en': 'Build a website',
   },
   'lng-telegram': {
     'es': 'Crear nuestro telegram',
-    'en': '',
+    'en': 'Create our Telegram',
   },
   'lng-tweeter': {
     'es': 'Crear nuestro tweeter',
-    'en': '',
+    'en': 'Create our Twitter',
   },
   'lng-instagram': {
     'es': 'Crear nuestro Instagram',
-    'en': '',
+    'en': 'Create our Instagram',
   },
   'lng-marketing': {
     'es': 'Empezar con las primeras campanas de marketing organicas',
-    'en': '',
+    'en': 'Start with the first organic marketing campaigns',
   },
 
   'lng-phase_two': {
@@ -57,27 +202,27 @@ export const langArr = {
   },
   'lng-presale': {
     'es': 'Realizar la Preventa',
-    'en': '',
+    'en': 'Conduct the Presale',
   },
   'lng-liquid': {
     'es': 'Crear el Liquidity Pol y quemarlo al instante',
-    'en': '',
+    'en': 'Create the Liquidity Pool and burn it instantly',
   },
   'lng-solana': {
     'es': 'Lanzamiento de $RODOLFO en Solana',
-    'en': '',
+    'en': 'Launch of $RODOLFO on Solana',
   },
   'lng-dex': {
     'es': 'Actualizar todos los datos en DexScreener',
-    'en': '',
+    'en': 'Update all data on DexScreener',
   },
   'lng-jupiter': {
     'es': 'Verificar nuestro token in RaYdium y Jupiter',
-    'en': '',
+    'en': 'Verify our token on Raydium and Jupiter',
   },
   'lng-pay': {
     'es': 'Marketing de pago en DexScreener',
-    'en': '',
+    'en': 'Paid marketing on DexScreener',
   },
 
   'lng-phase_three': {
@@ -86,57 +231,57 @@ export const langArr = {
   },
   'lng-promise': {
     'es': 'Realizar los objetivos segun el mardet cap alcanzado',
-    'en': '',
+    'en': 'Achieve the goals according to the market cap reached',
   },
   'lng-social': {
     'es': 'Realizar campanas de marketing de pago en redes sociales',
-    'en': '',
+    'en': 'Conduct paid marketing campaigns on social networks',
   },
   'lng-democrat': {
     'es': 'Votaciones con los miembros de la comunidad sobre la expansion del proyecto',
-    'en': '',
+    'en': 'Votes with community members on the expansion of the project',
   },
 
   'lng-teem': {
     'es': 'Equipo y Developers',
-    'en': '',
+    'en': 'Team and Developers',
   },
   'lng-token_pres': {
     'es': 'Preventa',
-    'en': '',
+    'en': 'Presale',
   },
   'lng-token_mark': {
     'es': 'Cartera de Marketing',
-    'en': '',
+    'en': 'Marketing Wallet',
   },
 
   'lng-plans__title': {
     'es': 'Planes segun la capitalizacion',
-    'en': '',
+    'en': 'Plans according to capitalization',
   },
   'lng-plans_one': {
     'es': '- Abriremos nuestra tienda de marketing con numerosos disenos de RODOLFO',
-    'en': '',
+    'en': '- We will open our marketing store with numerous RODOLFO designs',
   },
   'lng-plans_two': {
     'es': '- Comenzaremos con sorteos para los holders de RODOLFO',
-    'en': '',
+    'en': '- We will start with giveaways for RODOLFO holders',
   },
   'lng-plans_three': {
     'es': '- Crear numerosas vallas ublicitarias con la imagen de RODOLFO junto con campanas con influencers',
-    'en': '',
+    'en': '- Create numerous billboards with the image of RODOLFO along with influencer campaigns',
   },
   'lng-plans_four': {
     'es': '- Votaciones con los holders para decidir el futuro del proyecto',
-    'en': '',
+    'en': '- Votes with holders to decide the future of the project',
   },
 
   'lng-footer_disc': {
     'es': ' $RODOLFO es una memecoin sin valor intriseco ni expectativa de rendimiento financiero. $RODOLFO es completamente inutil y se ha creado con fines recreativos y de entretenimiento. Al comprar $RODOLFO, aceptas haber leido este descargo de responsabilidad.',
-    'en': '',
+    'en': ' $RODOLFO is a memecoin with no intrinsic value or expectation of financial return. $RODOLFO is completely useless and was created for recreational and entertainment purposes. By purchasing $RODOLFO, you agree to have read this disclaimer.',
   },
   'lng-footer_span': {
     'es': '®Aviso legal:',
-    'en': '',
+    'en': '®Legal notice:',
   },
 }
